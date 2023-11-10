897855b210aab9f8863d359eac7819442ad2c5cd
21120159 <vochitrung.pst@gmail.com>
2023-11-10 11:20:24 +0700

    crear connection pool

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connectionInfo = {
+const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-}
+});
+
 module.exports = {
   getAll: async (tbName) => {
-    const db = mysql.createConnection(connectionInfo);
     try {
       const sql = `select * from ${tbName}`;
       const result = await new Promise((resolve, reject) => {
-        db.query(sql, (err, data) => {
+        pool.query(sql, (err, data) => {
           if (err) {
             reject(err);
           } else {
@@ -24,18 +24,15 @@ module.exports = {
       return result;
     } catch (error) {
       throw error;
-    } finally {
-      db.end();
     }
   },
   getRandom: async (tbName, condition, limit) => {
-    const db = mysql.createConnection(connectionInfo);
     try {
       const limitSql = parseInt(limit)? parseInt(limit): "";
       const conditionSql = condition != ""? 'where ' + condition: '';
       const sql = `select * from ${tbName} ${conditionSql} ORDER BY RAND() ${limitSql}`;
       const result = await new Promise((resolve, reject) => {
-        db.query(sql, (err, data) => {
+        pool.query(sql, (err, data) => {
           if (err) {
             reject(err);
           } else {
@@ -46,8 +43,6 @@ module.exports = {
       return result;
     } catch (error) {
       throw error;
-    } finally {
-      db.end();
     }
   },
 };
